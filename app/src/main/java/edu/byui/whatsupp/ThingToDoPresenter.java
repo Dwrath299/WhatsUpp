18260007f6e7ea21859ba729fbe708e87f7d4f5f
Dallin Wrathall <Dwrath299@gmail.com>
2018-02-26 09:13:18 -0700

    Basic setup of MVP

---
+++ app/src/main/java/edu/byui/whatsupp/ThingToDoPresenter.java
@@ -0,0 +1,14 @@
+package edu.byui.whatsupp;
+
+/**
+ * Created by Dallin's PC on 2/26/2018.
+ */
+
+public class ThingToDoPresenter {
+    ThingToDoActivity thingToDoActivity;
+    ThingToDoData thingToDoData;
+
+    public ThingToDoPresenter() {
+
+    }
+}
---
+++ app/src/main/java/edu/byui/whatsupp/UserActivity.java
@@ -0,0 +1,13 @@
+package edu.byui.whatsupp;
+
+/**
+ * Created by Dallin's PC on 2/26/2018.
+ */
+
+public class UserActivity {
+    UserPresenter userPresenter;
+
+    public UserActivity() {
+
+    }
+}
