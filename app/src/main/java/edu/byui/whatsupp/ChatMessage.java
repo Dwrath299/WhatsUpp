e8491c546cf58a99100165bedb8c030e3d31b471
DESKTOP-MOI3VRL\Trent Buckner <buc14008@byui.edu>
2018-03-23 09:10:34 -0600

    Merge branch 'master' of https://github.com/Dwrath299/WhatsUpp into TrentBranch
    # Conflicts:
    #	app/src/main/java/edu/byui/whatsupp/ThingToDo.java
    #	app/src/main/java/edu/byui/whatsupp/ViewEvent.java
    #	app/src/main/java/edu/byui/whatsupp/ViewThingToDo.java

--- app/src/main/java/edu/byui/whatsupp/ChatMessage.java
+++ app/src/main/java/edu/byui/whatsupp/ChatMessage.java
@@ -6,12 +6,20 @@ import java.util.Date;
  * Created by Dallin's PC on 3/21/2018.
  */
 
+/**
+ * Contains all the information that is stored inside a chat message.
+ */
 public class ChatMessage {
 
     private String messageText;
     private String messageUser;
     private long messageTime;
 
+    /**
+     * Non-default Constructor.
+     * @param messageText
+     * @param messageUser
+     */
     public ChatMessage(String messageText, String messageUser) {
         this.messageText = messageText;
         this.messageUser = messageUser;
@@ -20,10 +28,17 @@ public class ChatMessage {
         messageTime = new Date().getTime();
     }
 
+    /**
+     * Default Constructor.
+     */
     public ChatMessage(){
 
     }
 
+    /**
+     * Getters and Setters.
+     * @return
+     */
     public String getMessageText() {
         return messageText;
     }
